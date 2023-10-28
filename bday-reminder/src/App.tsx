17d639326b9868679e2e5d9cd816eb22896ee396
violettomania <danielpolya@protonmail.com>
2023-10-28 15:55:55 +0200

    feature: display first item

--- bday-reminder/src/App.tsx
+++ bday-reminder/src/App.tsx
@@ -1,5 +1,20 @@
+import { portraits } from './data';
+
 function App() {
-  return <h2>reminder project setup</h2>;
+  return (
+    <main>
+      <div className='container'>
+        <h2>5 Birthdays Today</h2>
+        <div className='person'>
+          <img src={portraits[0].image} alt='portrait of a person' />
+          <div>
+            <h4>Bertie Yates</h4>
+            <p>29 years</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default App;
